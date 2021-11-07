4d1e0b29a90692b7f77f466897d060208cc7c395
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2021-11-07 21:06:48 +0300

    Test

---
+++ src/main/java/com/uni/Some.java
@@ -0,0 +1,15 @@
+package main.java.com.uni;
+
+public class Some {
+    private int number;
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int square(){
+        return number * number;
+    }
+
+    public int triple() {return 3 * number; }
+}
---
+++ src/test/java/com/uni/SomeTest.java
@@ -0,0 +1,24 @@
+package test.java.com.uni;
+
+import org.junit.jupiter.api.Test;
+
+import main.java.com.uni.Some;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SomeTest {
+
+    @Test
+    void square() {
+        Some obj = new Some();
+        obj.setNumber(5);
+        assertEquals(25, obj.square());
+    }
+
+    @Test
+    void triple() {
+        Some obj = new Some();
+        obj.setNumber(5);
+        assertEquals(15, obj.triple());
+    }
+}
