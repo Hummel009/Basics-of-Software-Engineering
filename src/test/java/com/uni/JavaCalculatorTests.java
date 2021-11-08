37d66277e7e24e83e886cd2dbeb85d29093834d6
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2021-11-08 19:39:38 +0300

    -headless

--- src/test/java/com/uni/JavaCalculatorTests.java
+++ src/test/java/com/uni/JavaCalculatorTests.java
@@ -2,7 +2,6 @@ package test.java.com.uni;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.BeforeClass;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,12 +9,6 @@ import main.java.com.uni.JavaCalculator;
 import main.java.com.uni.JavaCalculator.Operation;
 
 class JavaCalculatorTests {
-
-    @BeforeClass
-    public static void setupHeadlessMode() {
-        System.setProperty("java.awt.headless", "false");
-    }
-    
 	JavaCalculator calculator = new JavaCalculator();
 
 	@Test
