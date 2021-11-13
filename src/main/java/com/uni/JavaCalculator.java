f807e1e496f572821c2f205e8f33a38fe276a234
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2021-11-13 17:40:23 +0300

    dropdown

--- src/main/java/com/uni/JavaCalculator.java
+++ src/main/java/com/uni/JavaCalculator.java
@@ -13,29 +13,30 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 //класс базируется на JFrame и наследует его содержимое, ActionListener отвечает за отслеживание нажатой кнопки
-public class JavaCalculator implements ActionListener {
-	private JButton[] button = new JButton[32]; //все наши кнопки
+public class JavaCalculator extends JFrame implements ActionListener {
+	private final JButton[] button = new JButton[50]; //все наши кнопки
 	private Operation operation; //тип операции - сложение, вычитание и т.д.
 	private double output; //выводимые данные
 	private double input1; //вводимые данные
 	private double input2; //вводимые данные
-	private JTextField outputField = new JTextField(20); //поле вывода
-	private JPanel panel = new JPanel();  //сетка из кнопок
+	private final JTextField outputField = new JTextField(20); //поле вывода
+	private final JPanel panel = new JPanel();  //сетка из кнопок
 	private int notInclude;
+	boolean isExtended = false;
 
 	public static void main(String[] arg) {
 		new JavaCalculator();
 	}
 
 	public JavaCalculator() {
-		JFrame frame = new JFrame("Calculator v1.2.0");  //создаём окно и задаём заголовок
+		super("Calculator v1.2.0");  //создаём окно и задаём заголовок
 
 		//создаём кнопки про помощи цикла
-		for (int i = 0; i <= 31; i++) {
+		for (int i = 0; i <= 49; i++) {
 			button[i] = new JButton();
 		}
 
-		panel.setLayout(new GridLayout(8, 4)); //сетка из кнопок - в высоту и в ширину
+		panel.setLayout(new GridLayout(7, 4)); //сетка из кнопок - в высоту и в ширину
 
 		//настраиваем созданные кнопки, используя новый метод "registerButton"
 		registerButton(button[13], "C");
@@ -65,28 +66,41 @@ public class JavaCalculator implements ActionListener {
 
 		registerButton(button[20], "√");
 		registerButton(button[21], "^");
-		registerButton(button[22], "log");
+		registerButton(button[34], "^2");
+		registerButton(button[35], "^3");
+
+		registerHiddenButton(button[22], "log");
+		registerHiddenButton(button[24], "sin°");
+		registerHiddenButton(button[25], "cos°");
+		registerHiddenButton(button[26], "tg°");
+		registerHiddenButton(button[27], "ctg°");
+		registerHiddenButton(button[28], "arcsin");
+		registerHiddenButton(button[29], "arccos");
+		registerHiddenButton(button[30], "arctg");
+		registerHiddenButton(button[31], "arcctg");
+		registerHiddenButton(button[42], "10^");
+		registerHiddenButton(button[36], "lg");
+		registerHiddenButton(button[37], "ln");
+		registerHiddenButton(button[38], "ch");
+		registerHiddenButton(button[39], "sh");
+		registerHiddenButton(button[40], "th");
+		registerHiddenButton(button[41], "cth");
+
+		registerButton(button[43], "1/x");
 		registerButton(button[23], "n!");
+		registerButton(button[44], "n!!");
 
-		registerButton(button[24], "sin°");
-		registerButton(button[25], "cos°");
-		registerButton(button[26], "tg°");
-		registerButton(button[27], "ctg°");
-
-		registerButton(button[28], "arcsin");
-		registerButton(button[29], "arccos");
-		registerButton(button[30], "arctg");
-		registerButton(button[31], "arcctg");
+		registerButton(button[32], "Инженер");
 
 		outputField.setFont(outputField.getFont().deriveFont(40f)); //меняем размер шрифта полю вывода
 		outputField.setHorizontalAlignment(SwingConstants.RIGHT); //располагаем выводимый текст справа, а не по умолчанию в центре
 		outputField.setEditable(false); //запрещаем редактировать поле через мышь и клавиатуру, т.к. вычислять таким образом всё равно нельзя.
 
-		frame.add(outputField, BorderLayout.NORTH); //поле вывода - наверху
-		frame.add(panel, BorderLayout.CENTER); //кнопки - под полем вывода
-		frame.setVisible(true); //окно видимое
-		frame.setSize(600, 700); //размер окна
-		frame.setLocationRelativeTo(null); //выводим окно в центре экрана
+		add(outputField, BorderLayout.NORTH); //поле вывода - наверху
+		add(panel, BorderLayout.CENTER); //кнопки - под полем вывода
+		setVisible(true); //окно видимое
+		setSize(600, 700); //размер окна
+		setLocationRelativeTo(null); //выводим окно в центре экрана
 	}
 
 	/* Этот метод помогает сократить количество кода, выполняя сразу несколько действий. Аргументы:
@@ -100,20 +114,27 @@ public class JavaCalculator implements ActionListener {
 		button.setFont(button.getFont().deriveFont(20f)); //присваиваем кнопке увеличенный шрифт
 		button.addActionListener(this); //присваиваем кнопке механизм вызова actionPerformed
 		button.setText(name); //присваиваем кнопке name в качестве текста
-		panel.add(button); //располагаем кнопку на сетке кнопок.
+		panel.add(button);//располагаем кнопку на сетке кнопок.
+	}
+
+	public void registerHiddenButton(JButton button, String name) {
+		button.setFont(button.getFont().deriveFont(20f)); //присваиваем кнопке увеличенный шрифт
+		button.addActionListener(this); //присваиваем кнопке механизм вызова actionPerformed
+		button.setText(name); //присваиваем кнопке name в качестве текста
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		final JButton jbutton = (JButton) event.getSource(); //определяет, какая именно кнопка была нажата
 		selectButton(jbutton);
-	
 	}
-	
+
 	public void selectButton(JButton jbutton) {
 		//если нажатая кнопка - операция, то мы присваиваем ей операцию методами oneNumber или twoNumbers
 		if (jbutton == button[23]) {
 			oneNumber(Operation.FACTORIAL, button[23]);
+		}else if (jbutton == button[44]) {
+			oneNumber(Operation.DOUBLEFACT, button[44]);
 		} else if (jbutton == button[20]) {
 			oneNumber(Operation.SQRT, button[20]);
 		} else if (jbutton == button[24]) {
@@ -132,6 +153,24 @@ public class JavaCalculator implements ActionListener {
 			oneNumber(Operation.ARCTG, button[30]);
 		} else if (jbutton == button[31]) {
 			oneNumber(Operation.ARCCTG, button[31]);
+		}else if (jbutton == button[34]) {
+			oneNumber(Operation.SQAIR, button[34]);
+		} else if (jbutton == button[35]) {
+			oneNumber(Operation.KUB, button[35]);
+		} else if (jbutton == button[36]) {
+			oneNumber(Operation.LG, button[36]);
+		} else if (jbutton == button[38]) {
+			oneNumber(Operation.CH, button[38]);
+		} else if (jbutton == button[39]) {
+			oneNumber(Operation.SH, button[39]);
+		} else if (jbutton == button[40]) {
+			oneNumber(Operation.TH, button[40]);
+		} else if (jbutton == button[41]) {
+			oneNumber(Operation.CTH, button[41]);
+		} else if (jbutton == button[42]) {
+			oneNumber(Operation.TEN, button[42]);
+		} else if (jbutton == button[43]) {
+			oneNumber(Operation.BACK, button[43]);
 		} else if (jbutton == button[17]) {
 			twoNumbers(Operation.PLUS, button[17]);
 		} else if (jbutton == button[16]) {
@@ -146,18 +185,18 @@ public class JavaCalculator implements ActionListener {
 			twoNumbers(Operation.DIVIDE, button[14]);
 		} else if (jbutton == button[18]) {
 			twoNumbers(Operation.PERCENT, button[18]);
-		}
-
-		//если нажатая кнопка - очистить
-		else if (jbutton == button[13]) {
+		} else if (jbutton == button[32]) {
+			ing();
+			//если нажатая кнопка - очистить
+		} else if (jbutton == button[13]) {
 			clear();
 		}
-		
+
 		//если нажатая кнопка - равно
 		else if (jbutton == button[19]) {
 			equals();
 		}
-		
+
 		//если нажатая кнопка - число Эйлера
 		else if (jbutton == button[12]) {
 			outputField.setText("2.718281828459045"); //выводим число Эйлера
@@ -186,12 +225,63 @@ public class JavaCalculator implements ActionListener {
 		outputField.setText(""); //очищаем поле вывода
 	}
 
+	public void ing() {
+		if (!isExtended) {
+			panel.setLayout(new GridLayout(11, 4));
+			isExtended = true;
+			panel.remove(button[32]);
+
+			panel.add(button[42]);
+			panel.add(button[24]);
+			panel.add(button[25]);
+			panel.add(button[26]);
+			panel.add(button[27]);
+			panel.add(button[28]);
+			panel.add(button[29]);
+			panel.add(button[30]);
+			panel.add(button[31]);
+			panel.add(button[38]);
+			panel.add(button[39]);
+			panel.add(button[40]);
+			panel.add(button[41]);
+			panel.add(button[22]);
+			panel.add(button[36]);
+			panel.add(button[37]);
+
+			panel.add(button[32]);
+			button[32].setText("Назад");
+		} else {
+			panel.setLayout(new GridLayout(7, 4));
+			isExtended = false;
+			panel.remove(button[32]);
+
+			panel.remove(button[42]);
+			panel.remove(button[24]);
+			panel.remove(button[25]);
+			panel.remove(button[26]);
+			panel.remove(button[27]);
+			panel.remove(button[28]);
+			panel.remove(button[29]);
+			panel.remove(button[30]);
+			panel.remove(button[31]);
+			panel.remove(button[38]);
+			panel.remove(button[39]);
+			panel.remove(button[40]);
+			panel.remove(button[41]);
+			panel.remove(button[22]);
+			panel.remove(button[36]);
+			panel.remove(button[37]);
+
+			panel.add(button[32]);
+			button[32].setText("Инженер");
+		}
+	}
 
 	/* Действия, выполняемые кнопкой "равно" */
 	public void equals() {
 		input2 = Double.parseDouble(outputField.getText().substring(notInclude)); //считываем последние введённые данные
 		calculate(); //выполняем последнюю присвоенную операцию
-		String result = new DecimalFormat("#.###############").format(output); //округление
+		final String result = new DecimalFormat("#.###############").format(output); //округление
 		outputField.setText(outputField.getText() + "=" + result); //выводим результат
 	}
 
@@ -205,7 +295,7 @@ public class JavaCalculator implements ActionListener {
 		input1 = Double.parseDouble(outputField.getText()); //считываем введённые символы и преобразуем их в число
 		operation = op; //присваиваем операцию
 		calculate(); //выполняем вычисление, формула которого меняется в зависимости от операции
-		String result = new DecimalFormat("#.###############").format(output); //округление
+		final String result = new DecimalFormat("#.###############").format(output); //округление
 		outputField.setText(button.getText() + "(" + outputField.getText() + ")" + "=" + result); //выводим результат
 	}
 
@@ -280,52 +370,91 @@ public class JavaCalculator implements ActionListener {
 		case PERCENT:
 			output = input2 * input1 / 100;
 			break;
+		case SQAIR:
+			output = input1*input1;
+			break;
+		case KUB:
+			output = Math.pow(input1, 3);
+			break;
+		case LG:
+			output = Math.log10(input1);
+			break;
+		case LN:
+			output = Math.log(input1);
+			break;
+		case CH:
+			output =(Math.pow(2.7183, input1) + Math.pow(2.7183, (-1)*input1))/2;
+			break;
+		case SH:
+			output = (Math.pow(2.7183, input1) - Math.pow(2.7183, (-1)*input1))/2;
+			break;
+		case TH:
+			output = (Math.pow(2.7183, input1) - Math.pow(2.7183, (-1)*input1))/(Math.pow(2.7183, input1) + Math.pow(2.7183, (-1)*input1));
+			break;
+		case CTH:
+			output = (Math.pow(2.7183, input1) + Math.pow(2.7183, (-1)*input1))/(Math.pow(2.7183, input1) - Math.pow(2.7183, (-1)*input1));
+			break;
+		case TEN:
+			output = Math.pow(10,input1) ;
+			break;
+		case BACK:
+			output = 1/input1 ;
+			break;
+		case DOUBLEFACT:
+			long result2 = 1;
+			for (Long k = Math.round(input1); k>0; k = k - 2) {
+				result2 = result2*k;
+			}
+			output = result2;
+			break;
 		}
 		return 0;
 	}
 
 	//перечисление всех операций, где NULL - операция по умолчанию, использущаяся, если пользователь не присвоил новую операцию.
 	public enum Operation {
-		ARCCOS, ARCCTG, ARCSIN, ARCTG, COS, CTG, DIVIDE, FACTORIAL, LOGARITHM, MINUS, MULTIPLE, PERCENT, PLUS, POWER, SIN, SQRT, TG;
+		ARCCOS, ARCCTG, ARCSIN, ARCTG, COS, CTG, DIVIDE, FACTORIAL,
+		LOGARITHM, MINUS, MULTIPLE, PERCENT, PLUS, POWER, SIN,
+		SQRT, TG, SQAIR, KUB,LG,LN, CH, SH, TH, CTH, TEN, BACK,DOUBLEFACT;
 	}
-	
+
 	//доступ к приватному полю
 	public void addOutputText(String t) {
 		String o = outputField.getText();
 		o += t;
 		outputField.setText(o);
 	}
-	
+
 	//доступ к приватному полю
 	public void setInput1(double i) {
 		input1 = i;
 	}
-	
+
 	//доступ к приватному полю
 	public void setInput2(double i) {
 		input2 = i;
 	}
-	
+
 	//доступ к приватному полю
 	public String getOutputText() {
 		return outputField.getText();
 	}
-	
+
 	//доступ к приватному полю
 	public JButton getButton(int i) {
 		return button[i];
 	}
-	
+
 	//доступ к приватному полю
 	public int getNotInclude() {
 		return notInclude;
 	}
-	
+
 	//доступ к приватному полю
 	public double getOutput() {
 		return output;
 	}
-	
+
 	//доступ к приватному полю
 	public void setOperation(Operation op) {
 		operation = op;
