6006a966303460674013cffd51fa6645f6144653
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2021-11-30 17:58:05 +0300

    Update JavaCalculatorTests.java

--- src/test/java/com/uni/JavaCalculatorTests.java
+++ src/test/java/com/uni/JavaCalculatorTests.java
@@ -51,47 +51,4 @@ class JavaCalculatorTests {
 		calculator.selectButton(calculator.getButton(43));
 		assertEquals(1, calculator.getOutput());
 	}
-
-	@Test
-	void testSTH() {
-		calculator.selectButton(calculator.getButton(0));
-		calculator.selectButton(calculator.getButton(40));
-		assertEquals((Math.pow(2.7183, 0) - Math.pow(2.7183, (-1) * 0)) / (Math.pow(2.7183, 0) + Math.pow(2.7183, (-1) * 0)), calculator.getOutput());
-	}
-
-	@Test
-	void testSCTH() {
-		calculator.selectButton(calculator.getButton(3));
-		calculator.selectButton(calculator.getButton(41));
-		assertEquals((Math.pow(2.7183, 3) + Math.pow(2.7183, (-1) * 3)) / (Math.pow(2.7183, 3) - Math.pow(2.7183, (-1) * 3)), calculator.getOutput());
-	}
-
-	@Test
-	void testExtended() {
-		calculator.selectButton(calculator.getButton(32));
-		assertEquals(true, calculator.getExtended());
-		calculator.selectButton(calculator.getButton(32));
-		assertEquals(false, calculator.getExtended());
-	}
-
-	@Test
-	void testCLEAR() {
-		calculator.selectButton(calculator.getButton(3));
-		calculator.selectButton(calculator.getButton(13));
-		assertEquals(0, calculator.getOutput());
-	}
-
-	@Test
-	void testE() {
-		calculator.selectButton(calculator.getButton(12));
-		calculator.selectButton(calculator.getButton(19));
-		assertEquals(2.718281828459045, calculator.getOutput());
-	}
-
-	@Test
-	void testP() {
-		calculator.selectButton(calculator.getButton(11));
-		calculator.selectButton(calculator.getButton(19));
-		assertEquals(3.141592653589793, calculator.getOutput());
-	}
 }
