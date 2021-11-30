610946a2444665558c4d043597e599fc82529de0
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2021-11-30 18:04:39 +0300

    Update JavaCalculatorTests.java

--- src/test/java/com/uni/JavaCalculatorTests.java
+++ src/test/java/com/uni/JavaCalculatorTests.java
@@ -23,32 +23,4 @@ class JavaCalculatorTests {
 		calculator.selectButton(calculator.getButton(35));
 		assertEquals(27, calculator.getOutput());
 	}
-
-	@Test
-	void testSSQUARE() {
-		calculator.selectButton(calculator.getButton(3));
-		calculator.selectButton(calculator.getButton(34));
-		assertEquals(9, calculator.getOutput());
-	}
-
-	@Test
-	void testSSH() {
-		calculator.selectButton(calculator.getButton(3));
-		calculator.selectButton(calculator.getButton(39));
-		assertEquals((Math.pow(2.7183, 3) - Math.pow(2.7183, (-1) * 3)) / 2, calculator.getOutput());
-	}
-
-	@Test
-	void testSCH() {
-		calculator.selectButton(calculator.getButton(3));
-		calculator.selectButton(calculator.getButton(38));
-		assertEquals((Math.pow(2.7183, 3) + Math.pow(2.7183, (-1) * 3)) / 2, calculator.getOutput());
-	}
-
-	@Test
-	void testSBACK() {
-		calculator.selectButton(calculator.getButton(1));
-		calculator.selectButton(calculator.getButton(43));
-		assertEquals(1, calculator.getOutput());
-	}
 }
