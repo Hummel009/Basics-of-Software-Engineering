9215c1dd664209de17853d10175c7354a1269d75
Hummel009 <emperorbur@gmail.com>
2022-06-29 21:53:02 +0300

    update

--- DelphiPort/src/main/java/hummel/Lab1_02.java
+++ DelphiPort/src/main/java/hummel/Lab1_02.java
@@ -3,20 +3,52 @@ package main.java.hummel;
 import java.util.Scanner;
 
 public class Lab1_02 {
-	private static Scanner input;
+	private static Scanner input = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		int n = 0;
-		boolean error = false;
-		input = new Scanner(System.in);
+		boolean error;
 		do {
-			System.out.println("Enter the quantity of input array elements:");
+			error = false;
+			System.out.print("Enter the quantity of numbers: ");
 			try {
 				n = Integer.parseInt(input.nextLine());
 			} catch (NumberFormatException e) {
 				error = true;
 			}
-		} while (n <= 1 && error);
-		System.out.println(n);
+		} while (error);
+
+		int[] arr = new int[n];
+
+		for (int i = 0; i <= n - 1; i++) {
+			do {
+				error = false;
+				System.out.print("Enter the element: ");
+				try {
+					arr[i] = Integer.parseInt(input.nextLine());
+					if (arr[i] < 0) {
+						arr[i] *= -1;
+					}
+				} catch (NumberFormatException e) {
+					error = true;
+				}
+			} while (error);
+		}
+
+		for (int i = 0; i <= n - 2; i++) {
+			if (arr[i] != 0) {
+				do {
+					if (arr[i] < arr[i + 1]) {
+						arr[i + 1] = arr[i + 1] % arr[i];
+					} else {
+						int temp = arr[i];
+						arr[i] = arr[i + 1];
+						arr[i + 1] = temp;
+					}
+				} while (arr[i] != 0);
+			}
+		}
+
+		System.out.println("GCD is " + arr[n - 1]);
 	}
 }
