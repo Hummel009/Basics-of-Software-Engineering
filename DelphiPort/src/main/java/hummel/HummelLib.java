8867578b48f3090f243ebde71cc1f31e85e2db16
Hummel009 <emperorbur@gmail.com>
2022-07-06 21:16:32 +0300

    sus

--- DelphiPort/src/main/java/hummel/HummelLib.java
+++ DelphiPort/src/main/java/hummel/HummelLib.java
@@ -3,6 +3,18 @@ package main.java.hummel;
 import java.util.*;
 
 public class HummelLib {
+	public static void bubbleSort(int arr[], int arrSize) {
+		for (int i = 0; i < arrSize - 1; i++) {
+			for (int j = 0; j < arrSize - 1 - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+	}
+
 	public static int readNumberUntilNoError(Scanner input, String message) {
 		int n = 0;
 		boolean error;
