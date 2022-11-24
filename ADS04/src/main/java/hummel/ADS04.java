9b5ca09ce8423ab2d2a58c3099f7fcb2cff808b7
Hummel009 <emperorbur@gmail.com>
2022-11-24 21:31:30 +0300

    in progress

---
+++ ADS04/src/main/java/hummel/ADS04.java
@@ -0,0 +1,73 @@
+package main.java.hummel;
+
+import java.util.*;
+
+public class ADS04 {
+	public static void main(String[] args) {
+		List<Edge> edges = new ArrayList<>();
+		edges.add(new Edge(0, 1, 6));
+		edges.add(new Edge(1, 2, 7));
+		edges.add(new Edge(2, 0, 5));
+		edges.add(new Edge(2, 1, 4));
+		edges.add(new Edge(3, 2, 10));
+		edges.add(new Edge(4, 5, 1));
+		edges.add(new Edge(5, 4, 3));
+		Graph graph = new Graph(edges);
+		Graph.printGraph(graph);
+	}
+
+	public static class Edge {
+		public int src, dest, weight;
+
+		public Edge(int src, int dest, int weight) {
+			this.src = src;
+			this.dest = dest;
+			this.weight = weight;
+		}
+	}
+
+	public static class Graph {
+		public List<List<Node>> adjList = new ArrayList<>();
+
+		public Graph(List<Edge> edges) {
+			int n = 0;
+			for (Edge e : edges) {
+				n = Integer.max(n, Integer.max(e.src, e.dest));
+			}
+			for (int i = 0; i <= n; i++) {
+				adjList.add(i, new ArrayList<>());
+			}
+			for (Edge e : edges) {
+				adjList.get(e.src).add(new Node(e.dest, e.weight));
+			}
+		}
+
+		public static void printGraph(Graph graph) {
+			int src = 0;
+			int n = graph.adjList.size();
+
+			while (src < n) {
+				for (Node edge : graph.adjList.get(src)) {
+					System.out.printf("%d —> %s ", src, edge);
+				}
+
+				System.out.println();
+				src++;
+			}
+		}
+	}
+
+	public static class Node {
+		public int value, weight;
+
+		public Node(int value, int weight) {
+			this.value = value;
+			this.weight = weight;
+		}
+
+		@Override
+		public String toString() {
+			return value + " (" + weight + ")";
+		}
+	}
+}
