9bdf583cfb73eaca0ceb3a02b65b2373ddddaa09
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-01-20 17:18:40 +0300

    Update Ex0602.java

--- ExamLauncher/src/Ex0602.java
+++ ExamLauncher/src/Ex0602.java
@@ -1,56 +1,44 @@
 import java.util.Scanner;
 
-public class Ex0602
-{
+public class Ex0602 {
 	private static Node[] nodes;
 
-	public static void launch()
-	{
-		try (Scanner scanner = new Scanner(System.in))
-		{
+	private static boolean isTreeValid(int index, int leftBorder, int rightBorder) {
+		if (index == -1) {
+			return true;
+		}
+
+		if (nodes[index].value < leftBorder || nodes[index].value > rightBorder) {
+			return false;
+		}
+
+		return isTreeValid(nodes[index].left, leftBorder, nodes[index].value) && isTreeValid(nodes[index].right, nodes[index].value, rightBorder);
+	}
+
+	public static void launch() {
+		try (Scanner scanner = new Scanner(System.in)) {
 			int vertexCount = scanner.nextInt();
-			if (vertexCount != 0)
-			{
+			if (vertexCount != 0) {
 				nodes = new Node[vertexCount];
-				for (int i = 0; i < vertexCount; i++)
-				{
+				for (int i = 0; i < vertexCount; i++) {
 					nodes[i] = new Node(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
 				}
 
 				System.out.println(isTreeValid(0, Integer.MIN_VALUE, Integer.MAX_VALUE) ? "CORRECT" : "INCORRECT");
 			}
 
-			else
-			{
+			else {
 				System.out.println("CORRECT");
 			}
 		}
 	}
 
-	private static boolean isTreeValid(int index, int leftBorder, int rightBorder)
-	{
-		if (index == -1)
-		{
-			return true;
-		}
-
-		if (nodes[index].value < leftBorder || nodes[index].value > rightBorder)
-		{
-			return false;
-		}
-
-		return isTreeValid(nodes[index].left, leftBorder, nodes[index].value)
-			&& isTreeValid(nodes[index].right, nodes[index].value, rightBorder);
-	}
-
-	private static class Node
-	{
+	private static class Node {
 		int value;
 		int left;
 		int right;
 
-		private Node(int value, int left, int right)
-		{
+		private Node(int value, int left, int right) {
 			this.value = value;
 			this.left = left;
 			this.right = right;
