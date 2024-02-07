dc0602afe8c9730ed4ec9772883fea38ce9e6d61
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-07 12:54:21 +0300

    conventions 1

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "org.example"
-version = "v" + LocalDate.now().format(DateTimeFormatter.ofPattern("yy.MM.dd"))
+version = LocalDate.now().format(DateTimeFormatter.ofPattern("yy.MM.dd"))
 
 val embed: Configuration by configurations.creating
 
